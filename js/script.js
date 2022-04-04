a62620e3c3cc9ce0fe060379e2aa0c7c6dce9d38
Kryiz <antonkryiz@gmail.com>
2022-04-04 14:15:07 +0300

    fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//====================== Calc
+//====================== Calc, at work!
 var priceRepair;
 var priceHouse;
 var priceRoom;
@@ -8,9 +8,12 @@ function repairChecked(e) {
     var priceRepair = e.getAttribute("data-price");
     var priceHouse = e.getAttribute("data-p");
     var priceRoom = e.getAttribute("data-e");
-    // var result = priceRepair + priceHouse + priceRoom;
-    var result = -(e.getAttribute("data-price")) + -(e.getAttribute("data-p")) + -(e.getAttribute("data-e"));
-    document.getElementById('result').innerHTML = (result + ' р.'); // выводит id выбранного радио
+
+    var result = priceRepair + priceHouse + priceRoom;
+
+    // var result = +(e.getAttribute("data-price")) + +(e.getAttribute("data-p")) + +(e.getAttribute("data-e"));
+
+    document.getElementById('result').innerHTML = (result + ' р.');
 }
 
 //====================== fill and result Range
@@ -185,38 +188,24 @@ history.replaceState(null, null, ' ');
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    /* Записываем в переменные массив элементов-кнопок и подложку.
-       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
     var modalButtons = document.querySelectorAll('.js-open-modal'),
         overlay = document.querySelector('.js-overlay-modal'),
         closeButtons = document.querySelectorAll('.js-modal-close');
 
-
-    /* Перебираем массив кнопок */
     modalButtons.forEach(function (item) {
 
-        /* Назначаем каждой кнопке обработчик клика */
         item.addEventListener('click', function (e) {
 
-            /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-               люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-               Нужно подстраховаться. */
             e.preventDefault();
 
-            /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-               и будем искать модальное окно с таким же атрибутом. */
             var modalId = this.getAttribute('data-modal'),
                 modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
 
-
-            /* После того как нашли нужное модальное окно, добавим классы
-               подложке и окну чтобы показать их. */
             modalElem.classList.add('active');
             overlay.classList.add('active');
-        }); // end click
+        });
 
-    }); // end foreach
-
+    });
 
     closeButtons.forEach(function (item) {
 
@@ -227,8 +216,7 @@ document.addEventListener('DOMContentLoaded', function () {
             overlay.classList.remove('active');
         });
 
-    }); // end foreach
-
+    });
 
     document.body.addEventListener('keyup', function (e) {
         var key = e.keyCode;
@@ -246,7 +234,4 @@ document.addEventListener('DOMContentLoaded', function () {
         this.classList.remove('active');
     });
 
-
-
-
-}); // end ready
+});
